7293e0aa812b290973f054d78ea83603552fef25
YUNUS ÖZHAN <contactozhan@gmail.com>
2026-02-11 18:32:48 +0300

    Initial Commit

---
+++ TextBootcamp/TextBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextBootcampApp.swift
+//  TextBootcamp
+//
+//  Created by YUNUS ÖZHAN on 11.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct TextBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
